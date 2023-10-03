64fda2f163be70a5256ef3bfcbb642c615499d44
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-10-03 22:05:55 +0530

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Lahiru Amarasooriya on 2023-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Lahiru Amarasooriya on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
