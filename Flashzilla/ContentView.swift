6fc7a6fc6bf24882441b8d315c8a06d6458db290
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-10-11 23:24:26 +0530

    Moving views with DragGesture and offset()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -13,15 +13,28 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
             VStack {
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
-                        CardView(card: cards[index])
-                            .stacked(at: index, in: cards.count)
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                               removeCard(at: index)
+                           }
+                        }
+                        .stacked(at: index, in: cards.count)
                     }
                 }
             }
         }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
